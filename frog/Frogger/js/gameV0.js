5089e8533bd78447599f7dca706c4341ce273525
Melissa <hemiatlis@hotmail.com>
2021-07-09 14:11:57 +0200

    fog time

--- frog/Frogger/js/gameV0.js
+++ frog/Frogger/js/gameV0.js
@@ -24,6 +24,13 @@ let down, up, left, right;
 let haertIm;
 let tweenHeart;
 let carIm =[];
+let titleIm;
+let playIm;
+let onStartScreen = true;
+let minuteur;
+let timer;
+let counter = 60;
+
 
 let deadF;
 function init() {
@@ -40,6 +47,8 @@ function preload()
     this.load.image('dead', './assets/images/deadFrog.png');
     this.load.image('car1', './assets/images/F1-1.png');
     this.load.image('car2', './assets/images/snowCar.png');
+    this.load.image('title', './assets/images/TitleScreen.png');
+    this.load.image('play', './assets/images/playButton.webp');
 }
 
 function create() 
@@ -51,30 +60,31 @@ function create()
     let mumcase = Phaser.Math.Between(0,30) ;
     mumIm = this.add.image(mumcase*16,16, 'mum');
     mumIm.setOrigin(0, 0);
+
     
     for(let j = 0;j<3;j++){
-
+        
         for (let i = 0;i <30;i++)
         {
             let index = i+j*10;
-            let randomSpace = Phaser.Math.Between(-20,+25);
+            let randomSpace = Phaser.Math.Between(-15,+25);
             let randomCar =Phaser.Math.Between(0,2);
-            carIm[index] = this.physics.add.image(-50+i*50 + randomSpace, 190 + j*32, 'car'+randomCar);
+            carIm[index] = this.physics.add.image(-50+i*50 + randomSpace, 175 + j*37, 'car'+randomCar);
             carIm[index].setOrigin(0,0);
             carIm[index].setVelocity(30,0);
         }
     }
     for(let j = 0;j<3;j++){
-
+        
         for (let i = 0;i <30;i++)
         {
             let index = 30+i+j*10;
             let randomSpace = Phaser.Math.Between(-30,+15);
             let randomCar =Phaser.Math.Between(0,2);
-            carIm[index] = this.physics.add.image(480 +i*50 + randomSpace, 66 + j*32, 'car'+randomCar);
+            carIm[index] = this.physics.add.image(480 +i*50 + randomSpace, 66 + j*37, 'car'+randomCar);
             carIm[index].setOrigin(0,0);
             carIm[index].setAngle(-180);
-            carIm[index].setVelocity(-50,0);
+            carIm[index].setVelocity(-60,0);
         }
     }
     
@@ -88,43 +98,63 @@ function create()
         yoyo: false,
         loop: 0,
         paused: true
-        });
+    });
+    minuteur = this.add.text(10,10,counter,{fontFamily: 'Georgia', fontSize : 20, color : '#ffffff'})
+
+    timer = this.time.addEvent({
+        callback: coutdown,
+        callbackScope: this,
+        delay: 1000,
+        repeat: 59,
+        paused: false
+    });
     
+    titleIm = this.add.image(0,0,"title");
+    titleIm.setOrigin(0,0);
+    titleIm.setScale(0.7);
+
+    playIm = this.add.image(250,250,"play").setInteractive();
+    playIm.setScale(0.1)
+    playIm.on("pointerdown",startGame);
+
    
     down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP); 
     left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-       
 }
 
 function update()
 {
-    if (Phaser.Input.Keyboard.JustDown(down)&& frogIm.y < 304)
+    if(!onStartScreen)
     {
-        frogIm.y += 16 ;
-        frogIm.setAngle(180);
-    } 
-    
-    if (Phaser.Input.Keyboard.JustDown(up)&& frogIm.y > 16)
-    {
-        frogIm.y -= 16 ;
-        frogIm.setAngle(0);
-    } 
-    if (Phaser.Input.Keyboard.JustDown(left)&& frogIm.x > 16)
-    {
-        frogIm.x -= 16;
-        frogIm.setAngle(-90);
-    } 
-    if (Phaser.Input.Keyboard.JustDown(right)&& frogIm.x < 464)
-    {
-        frogIm.x += 16;
-        frogIm.setAngle(90);
-    } 
+        if (Phaser.Input.Keyboard.JustDown(down)&& frogIm.y < 304)
+        {
+            frogIm.y += 16 ;
+            frogIm.setAngle(180);
+        } 
+        
+        if (Phaser.Input.Keyboard.JustDown(up)&& frogIm.y > 16)
+        {
+            frogIm.y -= 16 ;
+            frogIm.setAngle(0);
+        } 
+        if (Phaser.Input.Keyboard.JustDown(left)&& frogIm.x > 16)
+        {
+            frogIm.x -= 16;
+            frogIm.setAngle(-90);
+        } 
+        if (Phaser.Input.Keyboard.JustDown(right)&& frogIm.x < 464)
+        {
+            frogIm.x += 16;
+            frogIm.setAngle(90);
+        } 
+
+    }
     
     if(Phaser.Geom.Intersects.RectangleToRectangle(frogIm.getBounds(),mumIm.getBounds() )) tweenHeart.play();
 
-    for (let i = 0;i <50;i++)
+    for (let i = 0;i <60;i++)
     {
         if(Phaser.Geom.Intersects.RectangleToRectangle(frogIm.getBounds(),carIm[i].getBounds()))
 
@@ -134,13 +164,20 @@ function update()
             frogIm.x =-100;
             
         }
-        if (i< 30&&carIm[i].x >500) carIm[i].x=-50;
-        if (i>30&&carIm[i].x <-50) carIm[i].x=500;
+        if (i< 30 && carIm[i].x >500) carIm[i].x=-50;
+        if (i>30 && carIm[i].x <-50) carIm[i].x=500;
 
     }   
 }
 
-function carback()
+function startGame()
 {
-    
+    titleIm.setVisible(false);
+    playIm.setVisible(false);
+    onStartScreen = false;
+}
+function coutdown()
+{
+    counter --;
+    timer.text = counter;
 }
